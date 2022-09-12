c78d7d165cb46f06d6ba94c704a45fe37b7af5e6
Rezaul Karim Shaon <rkshaon.ist@gmail.com>
2022-09-12 13:51:26 +0600

    dashboard page - done

--- video_share_pages/urls.py
+++ video_share_pages/urls.py
@@ -4,4 +4,5 @@ from video_share_pages import views
 
 urlpatterns = [
     path('', views.home_page, name='home'),
+    path('dashboard', views.dashboard_page, name='dashboard'),
 ]
--- video_share_pages/views.py
+++ video_share_pages/views.py
@@ -4,3 +4,8 @@ def home_page(request):
     context = {}
 
     return render(request, 'pages/home.html', context)
+
+def dashboard_page(request):
+    context = {}
+
+    return render(request, 'pages/dashboard.html', context)
