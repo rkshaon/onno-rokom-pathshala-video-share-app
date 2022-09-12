f35706999a58b96a847c064ad3d1730b6b088401
Rezaul Karim Shaon <rkshaon.ist@gmail.com>
2022-09-12 18:12:17 +0600

    user - log in - wip

--- static/js/script.js
+++ static/js/script.js
@@ -1,44 +1,3 @@
-// function signUpForm() {
-//     // console.log('Button clicked');
-//     let name = document.getElementById("user-name").value;
-//     let email = document.getElementById("user-email").value;
-//     let password1 = document.getElementById("user-password1").value;
-//     let password2 = document.getElementById("user-password2").value;
-
-//     // console.log(name);
-//     // console.log(email);
-//     // console.log(password1);
-//     // console.log(password2);
-//     if (password1 === password2) {
-//         let userData = {
-//             'name': name,
-//             'email': email,
-//             'password': password1
-//         }
-    
-//         console.log(userData);
-//     } else {
-//         alert('Password does not matched!');
-//         // location.href = ''
-//         location.reload();
-//     }    
-// }
-// $("#register-button").click(function(){
-//     alert('Button-hitted');
-// });
-
-// $("#register-button").click(function(){
-//     alert('Button');
-//     console.log('Hello button');
-//     // $.post("demo_test_post.asp",
-//     // {
-//     //   name: "Donald Duck",
-//     //   city: "Duckburg"
-//     // },
-//     // function(data, status){
-//     //   alert("Data: " + data + "\nStatus: " + status);
-//     // });
-//   });
 function getCookie(name) {
     let cookieValue = null;
     if (document.cookie && document.cookie !== '') {
@@ -54,6 +13,7 @@ function getCookie(name) {
     }
     return cookieValue;
 }
+
 const csrftoken = getCookie('csrftoken');
 
 $(document).ready(function(){
@@ -77,12 +37,8 @@ $(document).ready(function(){
             'csrfmiddlewaretoken': csrftoken
         }
 
-        // console.log(userData);
-
         $.post("http://127.0.0.1:8000/user-api/register", userData, function(data, status){
-            // alert("data: " + data + "\nstatus: ", status);
-            console.log(status);
-            console.log(data);
+            location.href = 'http://127.0.0.1:8000/login';
         });
     });
 });
