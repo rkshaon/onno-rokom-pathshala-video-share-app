829958a163b92a135a3660d4a0711612b683ad8d
Rezaul Karim Shaon <rkshaon.ist@gmail.com>
2022-09-13 00:51:18 +0600

    like dislike - model

--- video_api/admin.py
+++ video_api/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
 
 from video_api.models import Videoes
+from video_api.models import VideoLikeDislike
+
 
 admin.site.register(Videoes)
+admin.site.register(VideoLikeDislike)
--- video_api/models.py
+++ video_api/models.py
@@ -11,3 +11,13 @@ class Videoes(models.Model):
 
     def __str__(self):
         return str(self.uploaded_by.name) + " " + str(self.name) + " " + str(self.youtube_video_id)
+
+class VideoLikeDislike(models.Model):
+    video_id = models.ForeignKey('video_api.Videoes', on_delete=models.CASCADE)
+    like = models.BooleanField(default=False)
+    dislike = models.BooleanField(default=False)
+    given_by = models.ForeignKey('user_api.User', on_delete=models.CASCADE)
+    given_date_time = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return str(self.video_id.youtube_video_id) + " " + str(self.given_by.name)
